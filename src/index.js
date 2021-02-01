d715eb26848df198cddab78c26d4027a78166024
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-01 19:01:32 +0530

    used hooks to get current time

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//Challenge:
-//1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
-
 //2. Given that you can get code to be called every second
 //using the setInterval method.
 //Can you get the time in your <h1> to update every second?
